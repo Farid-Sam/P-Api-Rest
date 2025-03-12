2013f4c5a27634b74d11e013d75b5f16b919cbd6
Farid <saminoufarid2015@gmail.com>
2025-03-12 14:07:35 +0100

    Modification du dossier ai_health_assistant et suppression de certains fichiers

--- health/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AnalyzeMeal, RecommendExercise, DiagnoseSymptoms
-
-urlpatterns = [
-    path('nutrition/analyze/', AnalyzeMeal.as_view(), name='analyze_meal'),
-    path('exercise/recommend/', RecommendExercise.as_view(), name='recommend_exercise'),
-    path('symptoms/diagnose/', DiagnoseSymptoms.as_view(), name='diagnose_symptoms'),
-]
